58b7b7a97b423447e37d3995cfb30dbfd95db6ad
Charles M Hollins <68667434+Sp33dRunn3r@users.noreply.github.com>
2021-06-24 14:56:47 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+# These routes are more traditional to flask, and use less libraries to accomplish a similar goal for the project.
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
